40c3e13d2223f265e9cd907d00060aa3e8e5ead9
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-04-07 07:14:51 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { IntroductionComponent } from './introduction/introduction.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'intro', component: IntroductionComponent },
+    { path: '', redirectTo: '/intro', pathMatch: 'full' } 
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+export class AppRoutingModule { }
---
+++ src/app/introduction/introduction.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-introduction',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './introduction.component.html',
+  styleUrl: './introduction.component.css'
+})
+export class IntroductionComponent {
+
+}
