4212d8c4dde92b907bb57d24949e5829f2c45475
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-04-08 21:26:05 +0530

    Initial Commit

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-introduction',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule,FormsModule],
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.css'
 })
 export class IntroductionComponent {
+  fname: string = "Durgadas"; 
+  name: String = "Durgadas Joshi";
+  experience: number = 5; 
+  isDisabled: boolean = false;
+  activeTab: string = 'intro';
 
+  goToIntroduction(){
+    this.isDisabled = true;
+  }
 }
